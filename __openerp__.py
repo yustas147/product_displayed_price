9f9d1919e18a384e8468a0cf50a17e0155457073
yustas147 <yustas@simbioz.com.ua>
2018-02-08 08:54:53 +0200

    1st

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Product price adjust to round good-looking value with included taxes",
+    'author': "QArea, Yury Stasovsky",
+    'license': 'LGPL-3',
+    'website' : "https://qarea.us",
+    'category': 'Custom integration',
+    'version': '1.0.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'account'],
+
+    # always loaded
+    'data': [
+        'views/product_product.xml',
+            ],
+    'installable': True,
+}
